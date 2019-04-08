47a0eaf6c96f2ce1084b454dd462ea1f84d9d4d8
Germy Louis <germylouis@gmail.com>
2019-04-08 13:09:53 -0400

    connection made

--- project_zero/src/main/java/com/revature/main/BankDriver.java
+++ project_zero/src/main/java/com/revature/main/BankDriver.java
@@ -1,15 +1,45 @@
 package com.revature.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.revature.beans.*;
 import com.revature.dao.*;
+import com.revature.util.ConnectionUtil;
 
 public class BankDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 File file=new File("C:/gitrepos/Bank/project_zero/src/main/java/resources/Connection.prop");
+		 System.out.println(new File(".").getAbsoluteFile());
+		    System.out.println(file.exists());
+		    try {
+				Scanner scan = new Scanner(file);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  try 
+		  { 
+		  Connection con = ConnectionUtil.getConnectionFromFile("C:/gitrepos/Bank/project_zero/src/main/java/resources/Connection.prop");
+		  System.out.println(con); 
+		  }
+		  catch (SQLException e) 
+		  { 
+			  e.printStackTrace(); 
+		  }
+		  catch (IOException e) 
+		  {
+			  e.printStackTrace(); 
+		  }
+		 
 	}
 
 }
--- project_zero/src/main/java/com/revature/util/ConnectionUtil.java
+++ project_zero/src/main/java/com/revature/util/ConnectionUtil.java
@@ -9,16 +9,33 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-	// do not ever!!!! hardcode credentials like this
-		public static Connection getConnection() throws SQLException {
-			
-			return DriverManager.getConnection("", "", "");
-		}
+//	// do not ever!!!! hardcode credentials like this
+//		public static Connection getConnection() throws SQLException {
+//			
+//			return DriverManager.getConnection("", "", "");
+//		}
 		
 		public static Connection getConnectionFromFile(String filename) throws SQLException, IOException {
 			Properties prop = new Properties();
 			InputStream in = new FileInputStream(filename);
 			prop.load(in);
-			return DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("pass"));
+			return DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("password"));
 	}
+//	private static Connection connection;
+//	public static Connection getConnection() throws IOException, SQLException {
+//		Properties prop = new Properties();
+//		
+//		InputStream in = new FileInputStream("C:/gitrepos/Bank/project_zero/src/main/java/resources/Connection.prop");
+//		
+//		prop.load(in);
+//		String url = prop.getProperty("url");
+//		String username = prop.getProperty("username");
+//		String password = prop.getProperty("password");
+//		if (connection == null || connection.isClosed()) {
+//			connection = DriverManager.getConnection(url, username, password);
+//		}
+//		return connection;
+//	}
+
+		
 }
